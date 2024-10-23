ac169481fc2809c782ec8ad6dea5c90216a61f9e
levinhky <chansolle@gmail.com>
2024-10-23 16:54:19 +0700

    Update import scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "@/styles/Styles.module.scss";
+import "@/styles/Styles.scss";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
