b4af79b0932b6ad6916d1adc938f12801a4896ae
Hane <huongta.coltech@gmail.com>
2024-12-03 20:43:30 +0100

    Update list schedule

--- src/app/schedule/components/Calendar/ListCalendar/index.tsx
+++ src/app/schedule/components/Calendar/ListCalendar/index.tsx
@@ -87,12 +87,102 @@ const ListCalendar = ({
           },
         },
         {
-          title: '"Từ Đây... Từ Nay..." Slay Hà Nội (đêm 1)',
-          start: "2024-12-24",
+          title: '"Từ Đây... Từ Nay..." Slay Hà Nội (đêm 2)',
+          start: "2024-11-25",
           extendedProps: {
             address: "Hà Nội",
             category: "MINISHOW",
             id: 4,
+          },
+        },
+        {
+          title: 'Echoes of Love',
+          start: "2024-11-30",
+          extendedProps: {
+            address: "Paris Garden, T.P Hồ Chí Minh ",
+            category: "MINISHOW",
+            id: 5,
+          },
+        },
+        {
+          title: 'LiveStream Shoppee',
+          start: "2024-12-06",
+          extendedProps: {
+            address: "T.P Hồ Chí Minh ",
+            category: "OTHER",
+            id: 6,
+          },
+        },
+        {
+          title: 'SHOPEE SHOW',
+          start: "2024-12-11",
+          extendedProps: {
+            address: "T.P Hồ Chí Minh",
+            category: "OTHER",
+            id: 7,
+          },
+        },
+        {
+          title: 'ATVNCG',
+          start: "2024-12-14",
+          extendedProps: {
+            address: "Hà Nội",
+            category: "CONCERT",
+            id: 8,
+          },
+        },
+        {
+          title: 'Fan Meeting CHIN MUOI',
+          start: "2024-12-17",
+          extendedProps: {
+            address: "Hà Nội",
+            category: "OTHER",
+            id: 9,
+          },
+        },
+        {
+          title: '"Từ Đây... Từ Nay..." Slay Sài Gòn (đêm 1)',
+          start: "2024-12-20",
+          extendedProps: {
+            address: "T.P Hồ Chí Minh",
+            category: "MINISHOW",
+            id: 10,
+          },
+        },
+        {
+          title: '"Từ Đây... Từ Nay..." Slay Sài Gòn (đêm 2)',
+          start: "2024-12-21",
+          extendedProps: {
+            address: "T.P Hồ Chí Minh",
+            category: "MINISHOW",
+            id: 11,
+          },
+        },
+        {
+          title: 'Private Party',
+          start: "2024-12-22",
+          extendedProps: {
+            address: "T.P Hồ Chí Minh",
+            category: "MINISHOW",
+            id: 12,
+          },
+        },
+        {
+          title: '"Từ Đây... Từ Nay..." Slay Đà Nẵng',
+          start: "2024-12-26",
+          extendedProps: {
+            address: "Đà Nẵng",
+            category: "MINISHOW",
+            id: 13,
+          },
+        },
+        {
+          title: 'Bốn Mùa Yêu',
+          start: "2024-12-28",
+          extendedProps: {
+            address: "T.P Đà Lạt",
+            category: "CONCERT",
+            id: 14,
           },
         },
       ]}
