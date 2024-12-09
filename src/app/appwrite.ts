d81efbb1787e8d37ac229d4d9215437a728be383
levinhky <chansolle@gmail.com>
2024-12-09 11:38:45 +0700

    update config appwrite sdk

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("67566d5800398e6657ba");
+  .setEndpoint("https://appwrite.1202design.com/v1")
+  .setProject("675674020009e7e1580a");
 
 export const account = new Account(client);
 export { ID } from "appwrite";
