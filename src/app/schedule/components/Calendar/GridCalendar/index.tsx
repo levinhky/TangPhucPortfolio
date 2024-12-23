d2f358b569b7c99892fcb2d1e415311aa4b71294
levinhky <chansolle@gmail.com>
2024-12-23 16:06:41 +0700

    refactor code

--- src/app/schedule/components/Calendar/GridCalendar/index.tsx
+++ src/app/schedule/components/Calendar/GridCalendar/index.tsx
@@ -1,88 +1,14 @@
 "use client";
 import { LegacyRef, memo, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
+import CalendarDetailModal from "./components/CalendarDetailModal";
+
 import { DAYS_OF_WEEK, DEVICE, MOCK_DATA_EVENTS } from "@/utils/constants";
-import { formatVietnameseDate } from "@/utils/functions";
 
 import useDetectDevice from "@/hooks/useDetectDevice";
-
-const Modal = ({ onClose, calendarInfo }: any) => {
-  const { title, start, extendedProps } = calendarInfo?.event || {};
-  const { address, category, id, time } = extendedProps || {};
-  if (!calendarInfo) {
-    return null;
-  }
-
-  return (
-    <div className="absolute transform -top-10 -translate-x-1/2 w-[500px] max-w-2xl bg-custom-gradient rounded-lg shadow-lg border border-[#7096D1] backdrop-blur-md">
-      <div className="p-4 flex justify-between items-center border-b border-[#7096D1]">
-        <h3 className="text-lg flex gap-2 items-center justify-center font-semibold text-gray-800">
-          <Image
-            src={"/icons/calendar-blank.png"}
-            width={24}
-            height={24}
-            alt="calendar-blank"
-          />
-          {formatVietnameseDate(start)}
-        </h3>
-        <button onClick={onClose} className="text-gray-400 hover:text-gray-700">
-          <Image src={"/icons/close.png"} width={24} height={24} alt="close" />
-        </button>
-      </div>
-
-      {/* Events */}
-      <div className="p-4 space-y-4">
-        <div className="p-4 pt-0">
-          <h4 className="text-base font-semibold text-gray-800 mb-2">
-            {title}
-          </h4>
-          <div className="flex items-center justify-between">
-            <div>
-              <p className="text-sm text-gray-600 flex items-center gap-2">
-                <Image
-                  src={"/icons/clock.png"}
-                  width={24}
-                  height={24}
-                  alt="clock"
-                />
-                {time}
-              </p>
-              <p className="text-sm text-gray-600 flex items-center  gap-2 mt-1">
-                <Image
-                  src={"/icons/marker.png"}
-                  width={24}
-                  height={24}
-                  alt="marker"
-                />
-                {address}
-              </p>
-              {/* <p className="text-sm text-gray-500 mt-1 ml-7">
-                    {address}
-                  </p> */}
-            </div>
-            <Link
-              href={`/schedule/${id}`}
-              className="h-10 border-[#003EA0] border px-4 rounded-lg  flex gap-2 items-center justify-center text-textSecondaryTwo text-sm font-medium"
-            >
-              Xem chi tiết
-              <Image
-                src={"/icons/arrow-right-primary.png"}
-                width={24}
-                height={24}
-                alt="arrow-right"
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const GridCalendar = ({
   calendarRef,
@@ -95,6 +21,35 @@ const GridCalendar = ({
 
   const { device } = useDetectDevice();
 
+  const eventClassNames = (eventInfo: any) => {
+    return [
+      `bg-transparent border-0 ${
+        calendarInfo &&
+        eventInfo.event._instance?.instanceId ===
+          calendarInfo.event._instance?.instanceId &&
+        "!z-[10]"
+      }`,
+    ];
+  };
+
+  const eventContent = (eventInfo: any) => {
+    return (
+      <>
+        <p className="break-words overflow-hidden z-[4]">
+          {eventInfo.event.title}
+        </p>
+        {calendarInfo &&
+          eventInfo.event._instance?.instanceId ===
+            calendarInfo.event._instance?.instanceId && (
+            <CalendarDetailModal
+              calendarInfo={calendarInfo}
+              onClose={onClose}
+            />
+          )}
+      </>
+    );
+  };
+
   return (
     <div className="w-4/5 esm:w-full esm:mt-5 mx-auto">
       <div className="grid grid-cols-7 mb-7 rounded-lg border bg-custom-gradient border-[#003EA0]">
@@ -106,6 +61,7 @@ const GridCalendar = ({
           );
         })}
       </div>
+
       <FullCalendar
         ref={calendarRef}
         plugins={[dayGridPlugin]}
@@ -118,31 +74,9 @@ const GridCalendar = ({
           "!py-2 font-semibold !align-middle hidden !border-[#7096D1]"
         }
         viewClassNames={"grid-calendar-view"}
-        eventClassNames={(eventInfo) => {
-          return [
-            `bg-transparent border-0 ${
-              calendarInfo &&
-              eventInfo.event._instance?.instanceId ===
-                calendarInfo.event._instance?.instanceId &&
-              "!z-[10]"
-            }`,
-          ];
-        }}
+        eventClassNames={eventClassNames}
         eventTextColor={"#000054"}
-        eventContent={(eventInfo) => {
-          return (
-            <>
-              <p className="break-words overflow-hidden z-[4]">
-                {eventInfo.event.title}
-              </p>
-              {calendarInfo &&
-                eventInfo.event._instance?.instanceId ===
-                  calendarInfo.event._instance?.instanceId && (
-                  <Modal calendarInfo={calendarInfo} onClose={onClose} />
-                )}
-            </>
-          );
-        }}
+        eventContent={eventContent}
         showNonCurrentDates={false}
         height={"auto"}
         eventClick={(eventInfo) => {
--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -13,68 +13,68 @@ const Background = ({ isHomeType }: { isHomeType: boolean }) => {
   const { device } = useDetectDevice();
 
   return (
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 1376 770"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <mask id="svgMask">
-          <path
-            fill="white"
-            d="M1324 26C1324 20.4219 1324 17.6328 1323.45 15.3311C1321.69 8.01812 1315.98 2.30829 1308.67 0.552601C1306.37 0 1303.58 0 1298 0H40C21.1438 0 11.7157 0 5.85786 5.85786C0 11.7157 0 21.1438 0 40V730C0 748.856 0 758.284 5.85786 764.142C11.7157 770 21.1438 770 40 770H1336C1354.86 770 1364.28 770 1370.14 764.142C1376 758.284 1376 748.856 1376 730V78C1376 72.4219 1376 69.6328 1375.45 67.3311C1373.69 60.0181 1367.98 54.3083 1360.67 52.5526C1358.37 52 1355.58 52 1350 52C1344.42 52 1341.63 52 1339.33 51.4474C1332.02 49.6917 1326.31 43.9819 1324.55 36.6689C1324 34.3672 1324 31.5781 1324 26Z"
-          />
-        </mask>
-      </defs>
-      <foreignObject
+      <svg
         width="100%"
         height="100%"
-        mask={device === DEVICE.MOBILE ? "" : "url(#svgMask)"}
+        viewBox="0 0 1376 770"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <Swiper
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination, Autoplay]}
-          loop
-          className="h-full"
-          autoplay={{ delay: 5000, disableOnInteraction: false }}
-          id={isHomeType ? "homeSlider" : "backgroundSlider"}
-        >
-          {HERO_SWIPERS.map((item, index) => {
-            return (
-              <SwiperSlide key={`keySwiperSlide${index}`}>
-                <div className="w-full h-full">
-                  <Image
-                    src={item.src}
-                    alt="Background"
-                    fill
-                    className="object-cover aspect-1376/770"
-                    priority
-                  />
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        {isHomeType && (
-          <div className="z-10 flex flex-col items-center absolute bottom-2 left-2/4 -translate-x-2/4">
-            <Image
-              src={"/icons/mouse-scroll.png"}
-              width={24}
-              height={24}
-              id="mouseScroll"
-              alt="mouse-scroll"
+        <defs>
+          <mask id="svgMask">
+            <path
+              fill="white"
+              d="M1324 26C1324 20.4219 1324 17.6328 1323.45 15.3311C1321.69 8.01812 1315.98 2.30829 1308.67 0.552601C1306.37 0 1303.58 0 1298 0H40C21.1438 0 11.7157 0 5.85786 5.85786C0 11.7157 0 21.1438 0 40V730C0 748.856 0 758.284 5.85786 764.142C11.7157 770 21.1438 770 40 770H1336C1354.86 770 1364.28 770 1370.14 764.142C1376 758.284 1376 748.856 1376 730V78C1376 72.4219 1376 69.6328 1375.45 67.3311C1373.69 60.0181 1367.98 54.3083 1360.67 52.5526C1358.37 52 1355.58 52 1350 52C1344.42 52 1341.63 52 1339.33 51.4474C1332.02 49.6917 1326.31 43.9819 1324.55 36.6689C1324 34.3672 1324 31.5781 1324 26Z"
             />
-            <p className="text-white mt-2 text-[14px]">
-              Scroll down to view more
-            </p>
-          </div>
-        )}
-      </foreignObject>
-    </svg>
+          </mask>
+        </defs>
+        <foreignObject
+          width="1376"
+          height="770"
+          mask={device === DEVICE.MOBILE ? "" : "url(#svgMask)"}
+        >
+          <Swiper
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+            loop
+            className="h-full"
+            // autoplay={{ delay: 5000, disableOnInteraction: false }}
+            id={isHomeType ? "homeSlider" : "backgroundSlider"}
+          >
+            {HERO_SWIPERS.map((item, index) => {
+              return (
+                <SwiperSlide key={`keySwiperSlide${index}`}>
+                  <div className="w-full h-full">
+                    <Image
+                      src={item.src}
+                      alt="Background"
+                      fill
+                      className="object-cover aspect-1376/770"
+                      priority
+                    />
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          {isHomeType && (
+            <div className="z-10 flex flex-col items-center absolute bottom-2 left-2/4 -translate-x-2/4">
+              <Image
+                src={"/icons/mouse-scroll.png"}
+                width={24}
+                height={24}
+                id="mouseScroll"
+                alt="mouse-scroll"
+              />
+              <p className="text-white mt-2 text-[14px]">
+                Scroll down to view more
+              </p>
+            </div>
+          )}
+        </foreignObject>
+      </svg>
   );
 };
 
